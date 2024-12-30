52cbc1eeca59efde03b4c613f83250a05e288d5b
jngs-park <dear.jayp@gmail.com>
2024-12-30 18:40:18 +0900

    URL 처리기, RQ 클래스 기능 구현

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -9,6 +9,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test
+    public void Rq__getIntParam() {
+        Rq rq = new Rq("삭제?id=1"); // 위임해서 편의에 따라 기능을 구현
+
+        int id = rq.getIntParam("id", 0);
+        assertEquals(1, id);
+    }
+
+    @Test
+    public void Rq__getIntParam__2() {
+        Rq rq = new Rq("검색?id=10&no=1");
+        int id = rq.getIntParam("id", 0);
+        int no = rq.getIntParam("no", 0);
+        assertEquals(10, id);
+        assertEquals(1, no);
+    }
+
+    @Test
     public void 테스트_실험() {
         int rs = 10 + 20;
         assertEquals(30, rs);
@@ -17,10 +34,10 @@ public class AppTest {
     @Test
     public void 문자열을_스캐너의_입력으로_설정() {
         String input = """
-               등록
-               명언1
-               작가1
-               """.stripIndent();
+                등록
+                명언1
+                작가1
+                """.stripIndent();
         InputStream in = new ByteArrayInputStream(input.getBytes());
         Scanner sc = new Scanner(in);
 
@@ -31,8 +48,8 @@ public class AppTest {
         assertEquals("등록", cmd);
         assertEquals("명언1", content);
         assertEquals("작가1", author);
-
     }
+
 
     @Test
     public void 표준출력을_리다이렉션하여_결과를_문자열로_받기() throws IOException {
