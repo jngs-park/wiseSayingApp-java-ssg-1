2eec0a3120f59c82c2d8de43eec14b5a2f84a89b
jngs-park <dear.jayp@gmail.com>
2025-01-03 06:07:32 +0900

    try with resources 사용하여, 파일 관련 자원을 자동해제

--- src/main/java/com/ll/exam/Utill.java
+++ src/main/java/com/ll/exam/Utill.java
@@ -7,16 +7,13 @@ import java.nio.channels.FileChannel;
 
 public class Utill {
     public static void saveToFile(String path, String body) {
-        try {
-            RandomAccessFile stream = new RandomAccessFile(path, "rw");
-            FileChannel channel = stream.getChannel();
+        try (RandomAccessFile stream = new RandomAccessFile(path, "rw");
+             FileChannel channel = stream.getChannel()) {
             byte[] strBytes = body.getBytes();
             ByteBuffer buffer = ByteBuffer.allocate(strBytes.length);
             buffer.put(strBytes);
             buffer.flip();
             channel.write(buffer);
-            stream.close();
-            channel.close();
         } catch (IOException e) {
         }
     }
@@ -25,14 +22,13 @@ public class Utill {
         dir.mkdirs();
     }
     public static String getFromFile(String path) {
-        try {
-            RandomAccessFile reader = new RandomAccessFile(path, "r");
+        try (RandomAccessFile reader = new RandomAccessFile(path, "r")) {
             String body = reader.readLine();
-            reader.close();
             return body;
-        }
-        catch ( IOException e ) {
+        } catch (IOException e) {
+
         }
         return "";
     }
-}
+
+    }
