9737ef49646e49cf93d7ab15a010c9adb60b8e0c
jngs-park <dear.jayp@gmail.com>
2025-01-03 13:33:25 +0900

    테스트 클래스 분리, FileDBTest에서 매 테스트 전에 test_data 폴더 삭제

--- src/main/java/com/ll/exam/Util.java
+++ src/main/java/com/ll/exam/Util.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-public class Utill {
+public class Util {
     public static void saveToFile(String path, String body) {
         try (RandomAccessFile stream = new RandomAccessFile(path, "rw");
              FileChannel channel = stream.getChannel()) {
@@ -60,6 +63,15 @@ public class Utill {
         return map;
     }
 
+    public static void deleteDir(String path) {
+        Path rootPath = Paths.get(path);
+        try (Stream<Path> walk = Files.walk(rootPath)) {
+            walk.sorted(Comparator.reverseOrder())
+                    .map(Path::toFile)
+                    .forEach(File::delete);
+        } catch (IOException e) {
+        }
+    }
     }
 class Pair {
     // Return a map entry (key-value pair) from the specified values
---
+++ src/test/java/com/ll/exam/FileDBTest.java
@@ -0,0 +1,53 @@
+package com.ll.exam;
+import org.junit.jupiter.api.Test;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FileDBTest {
+
+    @Test
+    void 파일에_있는_JSON을_객체로_변환() {
+        Util.deleteDir("test_data");
+        Util.mkdir("test_data");
+        WiseSaying wiseSaying = new WiseSaying(1, "내 사전에 불가능은 없다.", "나폴레옹");
+        Util.saveToFile("test_data/1.json", wiseSaying.toJson());
+        String rs = Util.readFromFile("test_data/1.json");
+        Map<String, Object> map = Util.jsonToMap(rs);
+        WiseSaying loadedWiseSaying = new WiseSaying(map);
+        assertEquals(1, map.get("id"));
+        assertEquals("내 사전에 불가능은 없다.", map.get("content"));
+        assertEquals(wiseSaying, loadedWiseSaying);
+    }
+
+    @Test
+    void 파일에_있는_JSON을_맵으로_변환() {
+        Util.deleteDir("test_data");
+        Util.mkdir("test_data");
+        WiseSaying wiseSaying = new WiseSaying(1, "내 사전에 불가능은 없다.", "나폴레옹");
+        Util.saveToFile("test_data/1.json", wiseSaying.toJson());
+        String rs = Util.readFromFile("test_data/1.json");
+        Map<String, Object> map = Util.jsonToMap(rs);
+        assertEquals(1, map.get("id"));
+        assertEquals("내 사전에 불가능은 없다.", map.get("content"));
+        assertEquals("나폴레옹", map.get("author"));
+    }
+
+    @Test
+    void 파일에_객체를_저장() {
+        Util.deleteDir("test_data");
+        Util.mkdir("test_data");
+        WiseSaying wiseSaying = new WiseSaying(1, "내 사전에 불가능은 없다.", "나폴레옹");
+        Util.saveToFile("test_data/1.json", wiseSaying.toJson());
+        String rs = Util.readFromFile("test_data/1.json");
+        assertEquals(wiseSaying.toJson(), rs);
+    }
+
+    @Test
+    void 파일에_내용쓰기() {
+        Util.deleteDir("test_data");
+        Util.mkdir("test_data");
+        Util.saveToFile("test_data/1.json", "내용\n내용");
+        String rs = Util.readFromFile("test_data/1.json");
+        assertEquals("내용\n내용", rs);
+    }
+}
